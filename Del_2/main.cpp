2f240dd47897541c4efb300ab03a4d374787b30f
Daniel Shamany <dshamany@proton.me>
2025-11-20 22:20:32 +0100

    converterade projektet till CMake

--- Del_2/main.cpp
+++ Del_2/main.cpp
@@ -1,11 +1,10 @@
+#include "src/menu.h"
+
 #include <cstdlib>
 #include <ctime>
-#include <windows.h>
-#include "menu.h"
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    srand(time(nullptr));
-    Menu();
-    return 0;
+  srand(time(nullptr));
+  Menu();
+  return 0;
 }
